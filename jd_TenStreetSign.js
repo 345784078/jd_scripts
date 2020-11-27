ad6d5c0945107d0a1e7f47f6b1ba2de10b705946
twogen <mr.twogen@gmail.com>
2020-11-27 11:30:08 +0800

    update jd_TenStreetSign.js.

--- jd_TenStreetSign.js
+++ jd_TenStreetSign.js
@@ -44,7 +44,7 @@ function Ten_Street_Beans()
               url:FinalUrl,
               headers:HeadersWithCookies
           }
-   /*       
+          
         $httpClient.get(FullPar,function(error,response,data)
           {
               var StatusCode,JsonObj;
@@ -65,7 +65,7 @@ function Ten_Street_Beans()
                 $.msg("十元街领豆子-失败","您的Cookie信息可能已过期,请重新获取","Warning");
               }
           })
-          */
+          
       }
       else
       {
