455fad339e457ab78e39ac49c9d30fdd8c84ec2f
twogen <mr.twogen@gmail.com>
2021-01-13 09:27:59 +0800

    rename jd_cash.js to jd_cash_twogen.js.

--- jd_cash_twogen.js
+++ jd_cash_twogen.js
@@ -29,8 +29,8 @@ let cookiesArr = [], cookie = '', message;
 let helpAuthor = true;
 const randomCount = $.isNode() ? 20 : 5;
 const inviteCodes = [
-  `-4msulYas0O2JsRhE-2TA5XZmBQ@eU9Yar_mb_9z92_WmXNG0w@eU9YaO7jMvwh-W_VzyUX0Q`,
-  `-4msulYas0O2JsRhE-2TA5XZmBQ@eU9Yar_mb_9z92_WmXNG0w@eU9YaO7jMvwh-W_VzyUX0Q`
+  `eU9YaLnjMvsvoDrcyHJAgA@f0JqNe-0ZQ@eU9Yab63Y_sv-T-DznsU1Q@eU9YaeuyNPhy9GjcyXBG1A@eU9YaLnjMvsvoDrcyHJAgA`,
+  `eU9YaLnjMvsvoDrcyHJAgA@f0JqNe-0ZQ@eU9Yab63Y_sv-T-DznsU1Q@eU9YaeuyNPhy9GjcyXBG1A@eU9YaLnjMvsvoDrcyHJAgA`
 ]
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
