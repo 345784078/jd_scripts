546ebd964b7ec0340f4b32ba10d45e4b1f5352a4
twogen <mr.twogen@gmail.com>
2021-01-13 09:29:29 +0800

    rename jd_jxnc.js to jd_jxnc_twogen.js.

--- jd_jxnc_twogen.js
+++ jd_jxnc_twogen.js
@@ -44,7 +44,7 @@ let currentCookie = ''; // 当前用户 cookie
 let tokenNull = {'farm_jstoken': '', 'phoneid': '', 'timestamp': ''}; // 内置一份空的 token
 let tokenArr = []; // 用户 token 数组
 let currentToken = {}; // 当前用户 token
-const shareCode = '22bd6fbbabbaa770a45ab2607e7a1e8a@197c6094e965fdf3d33621b47719e0b1'; // 内置助力码
+const shareCode = '8cd5f6359979e6417d687b5f84c29980@69261707c951b39d026908df017ab4f8@f8ca2abdc4d65299a601852e950025b6@ff760cb16fe0b9b3aa55837d28d22900@e3dd6590e547ca51d18faab91d95490d'; // 内置助力码
 let jxncShareCodeArr = []; // 用户 助力码 数组
 let currentShareCode = []; // 当前用户 要助力的助力码
 const openUrl = `openjd://virtual?params=${encodeURIComponent('{ "category": "jump", "des": "m", "url": "https://wqsh.jd.com/sns/201912/12/jxnc/detail.html?ptag=7155.9.32&smp=b47f4790d7b2a024e75279f55f6249b9&active=jdnc_1_chelizi1205_2"}',)}`; // 打开京喜农场
